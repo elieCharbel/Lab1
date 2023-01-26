667cead0078b8ada66892717316cee008425209f
elieCharbel <eliejcharbel@outlook.com>
2023-01-26 20:26:17 +0200

    Initial Info Provided

---
+++ Profile.java
@@ -0,0 +1,8 @@
+public class Profile {
+    public static void main(String[] args) {
+        System.out.println("Full Name: Elie Joseph Charbel");
+        System.out.println("Major: Computer Engineering");
+        System.out.println("Year of Study: First Year.");
+
+    }
+}

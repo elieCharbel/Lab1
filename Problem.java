ff202b4c93c6e1b2e9db6944e6958a1ac65de510
elieCharbel <eliejcharbel@outlook.com>
2023-01-26 20:18:39 +0200

    Problem file is fixed by Elie Charbel

---
+++ Problem.java
@@ -0,0 +1,8 @@
+public class Problem {
+	public static void main(String[] args) {
+		System.out.println("Hi, can we check this?");
+		System.out.println("It has a lot of problems");
+		System.out.println("Yeah, I know !!!");
+		// This is a comment.
+	}
+}
